7144d9d054e7955f15bb76e984cd98f6e1cd848b
Árendás Ágnes <a.agnes2009@gmail.com>
2021-01-04 14:10:04 +0100

    todo app finished

--- toDo.js
+++ toDo.js
@@ -1,76 +1,112 @@
-import fs from 'fs';
+import fs from "fs";
 
 export class Todo {
-    constructor(tasks) {
-        this.tasks = tasks;
-    }
+  constructor(tasks) {
+    this.tasks = tasks;
+  }
 
-    getTasksList() {
-        try {
-            if (!fs.existsSync('./tasks.txt')) throw Error('File not exists');
-            this.tasksList = (fs.readFileSync('./tasks.txt', 'utf-8').split('\n'));
-        } catch (err) {
-            console.log(err.message);
-        }
+  getTasksList() {
+    try {
+      if (!fs.existsSync("./tasks.txt")) throw Error("File not exists");
+      this.tasksList = fs.readFileSync("./tasks.txt", "utf-8").split("\n");
+    } catch (err) {
+      console.log(err.message);
     }
+  }
 
-    listTasks() {
-        try {
-            if (!fs.existsSync('./tasks.txt')) throw Error('Unable to read file');
-            this.getTasksList();
-            if (this.tasksList.length === 0 || this.tasksList[0] === '') {
-                console.log('Nincs mára tennivalód! :)');
-            } else {
-                this.tasksList.forEach((task, index) => console.log(`${index + 1}. [ ] ${task}`));
-            }
-        } catch (err) {
-            console.log(err.message);
-        }
+  listTasks() {
+    try {
+      if (!fs.existsSync("./tasks.txt")) throw Error("Unable to read file");
+      this.getTasksList();
+      if (this.tasksList.length === 0 || this.tasksList[0] === "") {
+        console.log("Nincs mára tennivalód! :)");
+      } else {
+        this.tasksList.forEach((task, index) =>
+          console.log(`${index + 1}. ${task}`)
+        );
+      }
+    } catch (err) {
+      console.log(err.message);
     }
+  }
 
-    addNewTasks(newTasks) {
-        try {
-            if (!fs.existsSync('./tasks.txt')) throw Error('Unable to write file');
-            if (newTasks === '' || newTasks === true) throw Error('Nem lehetséges új feladat hozzáadása: nincs megadva feladat!');
-            fs.appendFileSync('./tasks.txt', '\n' + newTasks);
-        } catch (err) {
-            console.log(err.message);
-        }
+  addNewTasks(newTasks) {
+    try {
+      if (!fs.existsSync("./tasks.txt")) throw Error("Unable to write file");
+      if (newTasks === undefined || newTasks === true)
+        throw Error(
+          "Nem lehetséges új feladat hozzáadása: nincs megadva feladat!"
+        );
+      fs.appendFileSync("./tasks.txt", "\n" + "[ ] " + newTasks);
+    } catch (err) {
+      console.log(err.message);
     }
+  }
 
-    removeTasks(taskIndex) {
-        this.getTasksList();
-        try {
-            if (!fs.existsSync('./tasks.txt')) throw Error('File not exists');
-            if (taskIndex === '' || taskIndex === true) throw Error('Nem lehetséges az eltávolítás: nem adott meg indexet!');
-            if (taskIndex > this.tasksList.length) throw Error('Nem lehetséges az eltávolítás: túlindexelési probléma adódott!');
-            if (typeof taskIndex != "number") throw Error('Nem lehetséges az eltávolítás: a megadott index nem szám!');
-            if (taskIndex === 0) throw Error('Nem lehetséges az eltávolítás: a megadott index nem lehet 0!');
-            this.tasksList.splice(taskIndex - 1, 1);
-            fs.writeFileSync('./tasks.txt', this.tasksList.join("\n"));
-        } catch (err) {
-            console.log(err.message);
-        }
+  removeTasks(taskIndex) {
+    this.getTasksList();
+    try {
+      if (!fs.existsSync("./tasks.txt")) throw Error("File not exists");
+      if (taskIndex === undefined || taskIndex === true)
+        throw Error("Nem lehetséges az eltávolítás: nem adott meg indexet!");
+      if (taskIndex > this.tasksList.length)
+        throw Error(
+          "Nem lehetséges az eltávolítás: túlindexelési probléma adódott!"
+        );
+      if (typeof taskIndex != "number")
+        throw Error(
+          "Nem lehetséges az eltávolítás: a megadott index nem szám!"
+        );
+      if (taskIndex === 0)
+        throw Error(
+          "Nem lehetséges az eltávolítás: a megadott index nem lehet 0!"
+        );
+      this.tasksList.splice(taskIndex - 1, 1);
+      fs.writeFileSync("./tasks.txt", this.tasksList.join("\n"));
+    } catch (err) {
+      console.log(err.message);
     }
+  }
 
-    getTaskCompleted(taskIndex) {
-        this.getTasksList();
-        try {
-            if (!fs.existsSync('./tasks.txt')) throw Error('File not exists');
-            if (taskIndex === '' || taskIndex === true) throw Error('Nem lehetséges a feladat végrehajtása: nem adtál meg indexet!');
-            if (taskIndex > this.tasksList.length) throw Error('Nem lehetséges a feladat végrehajtása: túlindexelési probléma adódott!');
-            if (typeof taskIndex != "number") throw Error('Nem lehetséges a feladat végrehajtása: a megadott index nem szám!');
-            if (taskIndex === 0) throw Error('Nem lehetséges a feladat végrehajtása: a megadott index nem lehet 0!');
-            //this.tasksList.splice(taskIndex - 1, 1);
-            //fs.writeFileSync('./tasks.txt', this.tasksList.join("\n"));
-        } catch (err) {
-            console.log(err.message);
-        }
+  getTaskCompleted(taskIndex) {
+    this.getTasksList();
+    try {
+      if (!fs.existsSync("./tasks.txt")) throw Error("File not exists");
+      if (taskIndex === undefined || taskIndex === true)
+        throw Error(
+          "Nem lehetséges a feladat végrehajtása: nem adtál meg indexet!"
+        );
+      if (taskIndex > this.tasksList.length)
+        throw Error(
+          "Nem lehetséges a feladat végrehajtása: túlindexelési probléma adódott!"
+        );
+      if (typeof taskIndex != "number")
+        throw Error(
+          "Nem lehetséges a feladat végrehajtása: a megadott index nem szám!"
+        );
+      if (taskIndex === 0)
+        throw Error(
+          "Nem lehetséges a feladat végrehajtása: a megadott index nem lehet 0!"
+        );
+      let completedTask = this.tasksList[taskIndex - 1].replace("[ ]", "[X]");
+      this.tasksList[taskIndex - 1] = completedTask;
+      fs.writeFileSync("./tasks.txt", this.tasksList.join("\n"));
+    } catch (err) {
+      console.log(err.message);
     }
+  }
 
-    printUserManual() {
-        console.log("");
-        console.log(
-            'Parancssori Todo applikáció' + '\n=============================' + '\n' + '\nParancssori argumentumok:' + '\n-l   Kilistázza a feladatokat' + '\n-a   Új feladatot ad hozzá' + '\n-r   Eltávolít egy feladatot' + '\n-c   Teljesít egy feladatot')
-    };
+  printUserManual() {
+    console.log("");
+    console.log(
+      "Parancssori Todo applikáció" +
+        "\n=============================" +
+        "\n" +
+        "\nParancssori argumentumok:" +
+        "\n-l   Kilistázza a feladatokat" +
+        "\n-a   Új feladatot ad hozzá" +
+        "\n-r   Eltávolít egy feladatot" +
+        "\n-c   Teljesít egy feladatot"
+    );
+  }
 }
--- toDoApp.js
+++ toDoApp.js
@@ -1,32 +1,31 @@
-'use strict'
+"use strict";
 
-import minimist from 'minimist';
+import minimist from "minimist";
 
-import { Todo } from "./toDo.js"
+import { Todo } from "./toDo.js";
 
 const args = minimist(process.argv);
 
 let myTodo = new Todo();
 
 function todo() {
-
-    if (typeof (args._[2]) === "string" || typeof (args._[2]) === "number") {
-        console.log("");
-        console.log('Nem támogatott argumentum!');
-        myTodo.printUserManual();;
-    } else if (args.l === true) {
-        myTodo.listTasks();
-    } else if (args.hasOwnProperty('a')) {
-        myTodo.addNewTasks(args.a);
-    } else if (args.hasOwnProperty('r')) {
-        myTodo.removeTasks(args.r);
-    } else if (args.hasOwnProperty('c')) {
-        myTodo.getTaskCompleted(args.c)
-    } else {
-        myTodo.printUserManual();
-    }
+  if (typeof args._[2] === "string" || typeof args._[2] === "number") {
+    console.log("");
+    console.log("Nem támogatott argumentum!");
+    myTodo.printUserManual();
+  } else if (args.l === true) {
+    myTodo.listTasks();
+  } else if (args.hasOwnProperty("a")) {
+    myTodo.addNewTasks(args.a);
+  } else if (args.hasOwnProperty("r")) {
+    myTodo.removeTasks(args.r);
+  } else if (args.hasOwnProperty("c")) {
+    myTodo.getTaskCompleted(args.c);
+  } else {
+    myTodo.printUserManual();
+  }
 }
 
 todo();
 
-console.log(args);
+// console.log(args);
